049e43560a0e73acefd91b5ee7701bfaacebe8fe
Soham Jain <sjain64@ucsc.edu>
2026-04-01 19:09:40 -0700

    clean state

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Clean Slate</h1>
+        <p>Ready for a new project.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
